711a9256b7d048abc34b21243c37a7001dfddd9f
Peatech <90410753+Peatech@users.noreply.github.com>
2024-12-27 18:21:32 +0000

    Update fed_data_splitting.py

--- federated/fed_data_splitting.py
+++ federated/fed_data_splitting.py
@@ -1,7 +1,5 @@
-##############################################################
 # federated/fed_data_splitting.py
-# Provides a get_user_groups() function for IID or non-IID splits
-##############################################################
+
 import numpy as np
 
 def get_user_groups(dataset, num_users, iid=True):
@@ -12,7 +10,7 @@ def get_user_groups(dataset, num_users, iid=True):
 
     # Arguments
         dataset: A Dataset object with a .df DataFrame containing at least 'class_id' column
-        num_users: How many clients to split among
+        num_users: Number of federated clients
         iid: Boolean indicating whether to do IID or non-IID splits
     # Returns
         user_groups: dict -> {user_id: array of dataset indices}
@@ -33,22 +31,25 @@ def _iid_split(dataset, num_users):
 
     start = 0
     for uid in range(num_users):
-        user_groups[uid] = all_indices[start : start + num_items]
-        start += num_items
+        if uid != num_users - 1:
+            user_groups[uid] = all_indices[start : start + num_items]
+            start += num_items
+        else:
+            # Last user gets the remaining items
+            user_groups[uid] = all_indices[start:]
 
     return user_groups
 
 
 def _label_based_shard_split(dataset, num_users):
     """
-    Non-IID approach: We partition data by label shards.
-    This uses dataset.df['class_id'] for label-based splits.
+    Non-IID approach: Partition data by label shards.
     Each user gets shards of sorted label indices, 
     so data is concentrated in fewer classes per user.
 
     # Implementation details:
-    # - Suppose we want 2 shards per user => total_shards = num_users * 2
-    # - Sort the entire dataset by label => 'class_id'
+    # - Define #shards as num_users * shards_per_user (e.g., 2)
+    # - Sort the entire dataset by 'class_id'
     # - Assign consecutive shards to each user
     """
     df = dataset.df
@@ -58,9 +59,7 @@ def _label_based_shard_split(dataset, num_users):
     # Sort indices by class_id
     sorted_idxs = all_idxs[np.argsort(labels)]
 
-    # We'll define #shards as num_users * 2 (for example)
-    # You can also make this dynamic or pass as an argument
-    shards_per_user = 2
+    shards_per_user = 2  # Number of shards per user
     total_shards = num_users * shards_per_user
     shard_size = len(dataset) // total_shards
 
